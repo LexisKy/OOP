4939aeaf9e037cfee9cb37d3b642953243b091d3
LexisKky <alekseizainullin@gmail.com>
2022-06-21 12:33:03 +0300

    Добавьте файлы проекта.

---
+++ LR_1/replace/replace.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <optional>
+#include <fstream>
+
+const std::string WRONG_NUMBER_OF_ARGUMENTS = "Invalid number of arguments\n Use: replace.exe <input file> <output file> < search string> <replace string>\n";
+const int NUMBER_OF_ARGUMENTS = 5;
+
+struct Args
+{
+    std::string inputFileName;
+    std::string ouputFileName;
+    std::string searchString;
+    std::string replaceString;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+    if (argc != NUMBER_OF_ARGUMENTS)
+    {
+        std::cout << WRONG_NUMBER_OF_ARGUMENTS;
+
+        return std::nullopt;
+    }
+
+    Args args;
+    args.inputFileName = argv[1];
+    args.ouputFileName = argv[2];
+    args.searchString = argv[3];
+    args.replaceString = argv[4];
+
+    return args;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
